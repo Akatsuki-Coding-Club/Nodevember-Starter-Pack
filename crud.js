cba7f1f76b8d1b8a7907c7c7a117810c25e9763b
Pruthviraj Chaudhari <chaudharipruthviraj888@gmail.com>
2023-11-21 14:14:27 +0530

    Updated Crud.js

--- crud.js
+++ crud.js
@@ -1,65 +1,124 @@
-var personsData = [
-    {
-      Name: "John Doe",
-      Email: "john.doe@example.com",
-      Mobile: "1234567890",
-      Password: "password123"
+var PersonsData = [
+  {
+    Name: "Amit Sharma",
+    Email: "amit.sharma@example.com",
+    Mobile: "9876543210",
+    Password: "amitpass"
+  },
+  {
+    Name: "Priya Patel",
+    Email: "priya.patel@example.com",
+    Mobile: "8765432109",
+    Password: "priyapass"
+  },
+  {
+    Name: "Rahul Singh",
+    Email: "rahul.singh@example.com",
+    Mobile: "7654321098",
+    Password: "rahulpass"
+  },
+  {
+    Name: "Anjali Desai",
+    Email: "anjali.desai@example.com",
+    Mobile: "6543210987",
+    Password: "anjalipass"
+  },
+  {
+    Name: "Vikram Reddy",
+    Email: "vikram.reddy@example.com",
+    Mobile: "5432109876",
+    Password: "vikrampass"
+  },
+  {
+    Name: "Kavita Mehta",
+    Email: "kavita.mehta@example.com",
+    Mobile: "4321098765",
+    Password: "kavitapass"
+  },
+  {
+    Name: "Ravi Verma",
+    Email: "ravi.verma@example.com",
+    Mobile: "3210987654",
+    Password: "ravipass"
+  },
+  {
+    Name: "Neha Gupta",
+    Email: "neha.gupta@example.com",
+    Mobile: "2109876543",
+    Password: "nehapass"
+  },
+  {
+    Name: "Sandeep Yadav",
+    Email: "sandeep.yadav@example.com",
+    Mobile: "1098765432",
+    Password: "sandeepass"
+  },
+  {
+    Name: "Pooja Joshi",
+    Email: "pooja.joshi@example.com",
+    Mobile: "9876543210",
+    Password: "poojapass"
+  },
+  {
+      Name: "Arjun Kumar",
+      Email: "arjun.kumar@example.com",
+      Mobile: "8765432109",
+      Password: "arjunpass"
     },
     {
-      Name: "Jane Smith",
-      Email: "jane.smith@example.com",
+      Name: "Sneha Kapoor",
+      Email: "sneha.kapoor@example.com",
+      Mobile: "7654321098",
+      Password: "snehapass"
+    },
+    {
+      Name: "Rajat Singhania",
+      Email: "rajat.singhania@example.com",
+      Mobile: "6543210987",
+      Password: "rajatpass"
+    },
+    {
+      Name: "Deepika Rajput",
+      Email: "deepika.rajput@example.com",
+      Mobile: "5432109876",
+      Password: "deepikapass"
+    },
+    {
+      Name: "Vijay Mehra",
+      Email: "vijay.mehra@example.com",
+      Mobile: "4321098765",
+      Password: "vijaypass"
+    },
+    {
+      Name: "Shreya Nair",
+      Email: "shreya.nair@example.com",
+      Mobile: "3210987654",
+      Password: "shreyapass"
+    },
+    {
+      Name: "Aryan Khanna",
+      Email: "aryan.khanna@example.com",
+      Mobile: "2109876543",
+      Password: "aryanpass"
+    },
+    {
+      Name: "Ananya Sharma",
+      Email: "ananya.sharma@example.com",
+      Mobile: "1098765432",
+      Password: "ananyapass"
+    },
+    {
+      Name: "Rohan Malhotra",
+      Email: "rohan.malhotra@example.com",
       Mobile: "9876543210",
-      Password: "securepass"
+      Password: "rohanpass"
     },
     {
-      Name: "Alice Johnson",
-      Email: "alice.johnson@example.com",
-      Mobile: "5551112233",
-      Password: "alicepass"
-    },
-    {
-      Name: "Bob Williams",
-      Email: "bob.williams@example.com",
-      Mobile: "9998887777",
-      Password: "bobpass"
-    },
-    {
-      Name: "Eva Davis",
-      Email: "eva.davis@example.com",
-      Mobile: "4443332222",
-      Password: "evapass"
-    },
-    {
-      Name: "Michael Brown",
-      Email: "michael.brown@example.com",
-      Mobile: "7776665555",
-      Password: "michaelpass"
-    },
-    {
-      Name: "Olivia White",
-      Email: "olivia.white@example.com",
-      Mobile: "3332221111",
-      Password: "oliviapass"
-    },
-    {
-      Name: "Daniel Miller",
-      Email: "daniel.miller@example.com",
-      Mobile: "6665554444",
-      Password: "danielpass"
-    },
-    {
-      Name: "Sophia Wilson",
-      Email: "sophia.wilson@example.com",
-      Mobile: "2221110000",
-      Password: "sophiapass"
-    },
-    {
-      Name: "William Moore",
-      Email: "william.moore@example.com",
-      Mobile: "8887776666",
-      Password: "williampass"
+      Name: "Kirti Singh",
+      Email: "kirti.singh@example.com",
+      Mobile: "8765432109",
+      Password: "kirtipass"
     }
-  ];
-  
-  
-  db.Users.insertMany(personsData);
+];
+
+// db.users.insertMany(PersonsData);
